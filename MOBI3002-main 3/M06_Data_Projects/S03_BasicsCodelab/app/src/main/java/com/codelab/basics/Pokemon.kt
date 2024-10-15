7282c751a6b3d043a2476d6b708d799c7436a31b
minlcve <146142173+minlcve@users.noreply.github.com>
2024-10-15 02:46:55 -0300

    inital commit

---
+++ MOBI3002-main 3/M06_Data_Projects/S03_BasicsCodelab/app/src/main/java/com/codelab/basics/Pokemon.kt
@@ -0,0 +1,13 @@
+package com.codelab.basics
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Pokemon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
